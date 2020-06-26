e4dd122b161986613178fc2a8bbae741c380407e
wzb2020 <67461615+wzb2020@users.noreply.github.com>
2020-06-26 22:56:43 +0800

    合并两个有序链表
    将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。

---
+++ 合并两个有序链表.c
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
